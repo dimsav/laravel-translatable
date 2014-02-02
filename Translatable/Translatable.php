a9f52e89d740f3380a9f01af81f904450e6981ee
Dimitrios Savvopoulos <ds@dimsav.com>
2014-02-02 20:34:54 +0100

    Getting translations automagically

--- Translatable/Translatable.php
+++ Translatable/Translatable.php
@@ -1,11 +1,13 @@
 <?php namespace Dimsav\Translatable;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 abstract class Translatable extends Eloquent {
 
     public $translationModel;
+    public $translationForeignKey;
+    public $localeKey = 'locale';
+
     protected $translatedAttributes = array();
     protected $translationModels = array();
 
@@ -15,6 +17,29 @@ abstract class Translatable extends Eloquent {
 
     public function getTranslationModelNameDefault() {
         return get_class($this) . 'Translation';
+    }
+
+    public function getRelationKey() {
+        return $this->translationForeignKey ?: $this->getForeignKey();
+    }
+
+    public function getTranslationModel($locale = null) {
+        $locale = $locale ?: \App::getLocale();
+
+        if (isset ($this->translationModels[$locale])) {
+            return $this->translationModels[$locale];
+        }
+
+        $translation = $this->hasMany($this->getTranslationModelName())
+            ->where($this->localeKey, '=', $locale)
+            ->first();
+        return $this->translationModels[$locale] = $translation;
+    }
+
+    public function getAttribute($key) {
+        return in_array($key, $this->translatedAttributes) ?
+            $this->getTranslationModel()->$key :
+            parent::getAttribute($key);
     }
 
 }
--- tests/TranslatableTest.php
+++ tests/TranslatableTest.php
@@ -5,14 +5,14 @@ use Dimsav\Translatable\Test\Model\Country;
 class TranslatableTests extends TestsBase {
 
     public function testTranslationModelName() {
-        $country = new Country();
+        $country = new Country;
         $this->assertEquals(
             'Dimsav\Translatable\Test\Model\CountryTranslation',
             $country->getTranslationModelNameDefault());
     }
 
     public function testTranslationModelCustomName() {
-        $country = new Country();
+        $country = new Country;
 
         $this->assertEquals(
             $country->getTranslationModelNameDefault(),
@@ -26,7 +26,38 @@ class TranslatableTests extends TestsBase {
         );
     }
 
+    public function testRelationKey() {
+        $country = new Country;
 
+        $this->assertEquals('country_id', $country->getRelationKey());
 
+        $country->translationForeignKey = 'my_awesome_key';
+        $this->assertEquals('my_awesome_key', $country->getRelationKey());
+    }
 
-} 
+    public function testGettingTranslationModel() {
+        /** @var Country $country */
+        $country = Country::where('iso', '=', 'gr')->first();
+
+        $englishTranslation = $country->getTranslationModel('el');
+        $this->assertEquals('Ελλάδα', $englishTranslation->name);
+
+        $englishTranslation = $country->getTranslationModel('en');
+        $this->assertEquals('Greece', $englishTranslation->name);
+
+        $this->app->setLocale('el');
+        $englishTranslation = $country->getTranslationModel();
+        $this->assertEquals('Ελλάδα', $englishTranslation->name);
+
+        $this->app->setLocale('en');
+        $englishTranslation = $country->getTranslationModel();
+        $this->assertEquals('Greece', $englishTranslation->name);
+    }
+
+    public function testGettingAttributeFromTranslation() {
+        $this->app->setLocale('en');
+        $country = Country::where('iso', '=', 'gr')->first();
+
+        $this->assertEquals('Greece', $country->name);
+    }
+}
