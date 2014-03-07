a73f91a565e04d4569f23d02d45569cd675cbfb5
Samuel De Backer <sdebacker@gmail.com>
2014-03-07 08:42:26 +0100

    Deleting tests removed because deleting is done by DB

--- tests/TestCoreModelExtension.php
+++ tests/TestCoreModelExtension.php
@@ -56,36 +56,6 @@ class TestCoreModelExtension extends TestsBase {
         $country->fill(['en' => ['name' => 'Italy']]);
     }
 
-    // Deleting
-
-    public function testDeleting()
-    {
-        $country = Country::find(1);
-        $countryId = $country->id;
-        $translation = $country->en;
-        $this->assertTrue(is_object($translation));
-        $country->delete();
-        $country = Country::find($countryId);
-        $this->assertNull($country);
-
-        $translations = CountryTranslation::where('country_id', '=', $countryId)->get();
-        $this->assertEquals(0, count($translations));
-    }
-
-    public function testDeletingWithSoftDeleteDoesNotDeleteTranslations()
-    {
-        $country = CountryStrict::find(1);
-        $before = CountryTranslation::where('country_id', '=', 1)->get();
-        $country->delete();
-
-        $after = CountryTranslation::where('country_id', '=', 1)->get();
-        $this->assertEquals(count($before), count($after));
-
-        $country->forceDelete();
-        $after = CountryTranslation::where('country_id', '=', 1)->get();
-        $this->assertEquals(0, count($after));
-    }
-
     // Performance
 
     public function testNPlusOne()
