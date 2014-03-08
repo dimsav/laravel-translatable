f630f0b3c6dac5d4da656a6667c3186c83b05844
Samuel De Backer <sdebacker@gmail.com>
2014-03-08 14:37:57 +0100

    Added City model with foreign key constraint on Country

--- tests/migrations/2013_11_28_152610_create_tables.php
+++ tests/migrations/2013_11_28_152610_create_tables.php
@@ -31,6 +31,26 @@ class CreateTables extends Migration {
             $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
         });
 
+        Schema::create('cities', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('country_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('country_id')->references('id')->on('countries');
+        });
+
+        Schema::create('city_translations', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('city_id')->unsigned();
+            $table->string('name');
+            $table->string('locale')->index();
+
+            $table->unique(['city_id', 'locale']);
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
+        });
+
 	}
 
 
@@ -41,8 +61,10 @@ class CreateTables extends Migration {
 	 */
 	public function down()
 	{
+        Schema::drop('city_translations');
+        Schema::drop('cities');
         Schema::drop('country_translations');
-		Schema::drop('countries');
+        Schema::drop('countries');
 	}
 
 }
---
+++ tests/models/CityTranslation.php
@@ -0,0 +1,9 @@
+<?php namespace Dimsav\Translatable\Test\Model;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class CityTranslation extends Eloquent {
+
+    public $timestamps = false;
+
+}
