f772d7698a587cd97a710a81905eadf41ec55773
Samuel De Backer <sdebacker@gmail.com>
2014-03-08 12:44:56 +0100

    Added deleting tests

--- tests/TestCoreModelExtension.php
+++ tests/TestCoreModelExtension.php
@@ -56,6 +56,40 @@ class TestCoreModelExtension extends TestsBase {
         $country->fill(['en' => ['name' => 'Italy']]);
     }
 
+    // Deleting
+
+    public function testDeleting()
+    {
+        $country = Country::find(1);
+        $countryId = $country->id;
+        $translation = $country->en;
+        $this->assertTrue(is_object($translation));
+        $country->delete();
+        $country = Country::find($countryId);
+        $this->assertNull($country);
+
+        $translations = CountryTranslation::where('country_id', '=', $countryId)->get();
+        $this->assertEquals(0, count($translations));
+    }
+
+    public function testDeletingWithSoftDeleteDoesNotDeleteTranslations()
+    {
+        $country = CountryStrict::find(1);
+        $before = CountryTranslation::where('country_id', '=', 1)->get();
+        $country->delete();
+
+        $after = CountryTranslation::where('country_id', '=', 1)->get();
+        $this->assertEquals(count($before), count($after));
+    }
+
+    public function testForceDeletingWithSoftDeleteDoesDeleteTranslations()
+    {
+        $country = CountryStrict::find(1);
+        $country->forceDelete();
+        $after = CountryTranslation::where('country_id', '=', 1)->get();
+        $this->assertEquals(0, count($after));
+    }
+
     // Performance
 
     public function testNPlusOne()
