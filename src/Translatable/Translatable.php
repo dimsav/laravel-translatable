58447046e2330b929192418e77135637c3f9a997
Johannes Schobel <johannesschobel@users.noreply.github.com>
2016-08-04 15:09:19 +0200

    forget/syncTranslation()
    adds the following functions to the trait:
    forgetTranslations() - remove all translations from the model
    forgetTranslation(array) - removes the given translations from the model
    syncTranslation(array) - removes all but these translations from the model

--- src/Translatable/Translatable.php
+++ src/Translatable/Translatable.php
@@ -624,4 +624,58 @@ trait Translatable
         return App::make('config')->get('translatable.locale')
             ?: App::make('translator')->getLocale();
     }
+
+    /**
+     * Removes all translations for this model.
+     */
+    public function forgetTranslations()
+    {
+        $modelTranslation = $this->getTranslationModelName();
+        $modelTranslation::where($this->getRelationKey(), "=", $this->id)->delete();
+
+        // we need to manually "reload" the collection built from the relationship
+        // otherwise $this->translations()->get() would NOT be the same as $this->translations
+        $this->load("translations");
+    }
+
+    /**
+     * Removes one translation for this model.
+     *
+     * @param mixed $locales    The locales to be deleted (array or single string)
+     *                          (e.g., ["en", "de"] would remove these translations).
+     */
+    public function forgetTranslation($locales)
+    {
+        if(!is_array($locales)) {
+            $locales = array($locales);
+        }
+
+        $modelTranslation = $this->getTranslationModelName();
+        $modelTranslation::where($this->getRelationKey(), "=", $this->id)->whereIn($this->getLocaleKey(), $locales)->delete();
+
+        // we need to manually "reload" the collection built from the relationship
+        // otherwise $this->translations()->get() would NOT be the same as $this->translations
+        $this->load("translations");
+    }
+
+    /**
+     * Deletes the translations for this model, which are not listed in $locales.
+     *
+     * @param mixed $locales    The locales to be left untouched (array or single string)
+     *                          (e.g., ["en", "de"] would remove all locales but these).
+     */
+    public function syncTranslations($locales)
+    {
+        if(!is_array($locales)) {
+            $locales = array($locales);
+        }
+
+        $modelTranslation = $this->getTranslationModelName();
+        $modelTranslation::where($this->getRelationKey(), "=", $this->id)->whereNotIn($this->getLocaleKey(), $locales)->delete();
+
+        // we need to manually "reload" the collection built from the relationship
+        // otherwise $this->translations()->get() would NOT be the same as $this->translations
+        $this->load("translations");
+    }
+
 }
