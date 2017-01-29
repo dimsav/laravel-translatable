1528422b825d1accd52f709238f6c7e27a6029de
Dimitris Savvopoulos <ds@dimsav.com>
2017-01-29 19:01:09 +0100

    V7 (#317)
    * Updated composer
    
    * Fixing tests
    
    * Updated ci tests
    
    * Updated ci tests
    
    * fixed tests migrations

--- tests/TestsBase.php
+++ tests/TestsBase.php
@@ -36,13 +36,13 @@ class TestsBase extends TestCase
 
     private function makeSureSchemaIsCreated()
     {
-        $migrationsPath = __DIR__.'/migrations';
+        $migrationsPath = '../../../../tests/migrations';
         $artisan = $this->app->make('Illuminate\Contracts\Console\Kernel');
 
         // Makes sure the migrations table is created
         $artisan->call('migrate', [
             '--database' => 'mysql',
-            '--realpath'     => $migrationsPath,
+            '--path'     => $migrationsPath,
         ]);
     }
 
