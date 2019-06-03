62302e1b4472729c2593fea74d0d7d1baf9bb4ba
Gummibeer <dev.gummibeer@gmail.com>
2019-06-03 09:19:27 +0200

    fix php cs

--- src/Translatable/Translatable.php
+++ src/Translatable/Translatable.php
@@ -2,14 +2,14 @@
 
 namespace Dimsav\Translatable;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Query\JoinClause;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Query\Builder as QueryBuilder;
-use Illuminate\Support\Str;
 
 /**
  * @property-read Collection|Model[] $translations
